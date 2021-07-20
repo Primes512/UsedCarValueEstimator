2ea9b7e3f11ff7b35d0448b65b6e81f31771a3c7
Cruz Lopez <64482677+Primes512@users.noreply.github.com>
2021-07-20 15:51:42 -0500

    new file "rates"

---
+++ public/rates.js
@@ -0,0 +1,10 @@
+var rates = {
+    aluminum: 0.26,
+    castIron: 0.15,
+    transmission: 0.20,
+    wheel: 18,
+    battery: 0.28,
+    starter: 0.55,
+    acCompressor: 0.38,
+    alternator: 0.65
+}
